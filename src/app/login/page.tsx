0354e7a395404883d80d515b598e669c23a9cb32
Noor <thenoorulhassan.dev@gmail.com>
2026-03-02 04:39:37 +0500

    Login Success frontend page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,60 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { Axios } from "axios";
+import  axios from "axios";
+import toast from "react-hot-toast";
+import { log } from "console";
+
+
 
 export default function LoginPage() {
+  const router=useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
-  const onLogin = async () => {};
+  const [buttonDisabled,setButtonDisabled]=React.useState(false);
+  const [loading,setLoading]=React.useState(false);
+   useEffect(()=>{
+      if(user.email.length>0 && user.password.length>0)
+      {
+        setButtonDisabled(false);
+      }
+      else{
+        setButtonDisabled(true);
+      }
+
+    },[user])
+
+
+  const onLogin = async () => {
+
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login",user);
+      console.log("Success",response.data);
+      
+      toast.success("Login Success");
+      router.push("/profile")
+
+
+      
+    } catch (error:any) {
+      console.log("Login Failed",error.message);
+      toast.error(error.message);
+    } finally{
+      setLoading(false)
+    }
+
+   
+  };
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#0f172a] selection:bg-indigo-500/30">
       <div className="w-full max-w-md p-10 bg-[#1e293b]/50 backdrop-blur-xl border border-white/10 rounded-3xl shadow-2xl">
         <div className="mb-10 text-center">
           <h1 className="text-4xl font-extrabold text-white tracking-tight">
-            Login
+            {loading?"Processing..":"Login"}
           </h1>
           <p className="mt-2 text-slate-400">Start your journey today.</p>
         </div>
